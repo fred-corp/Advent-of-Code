60a38763f6c7edfc7f291591a3f3c681f23551f4
fred-corp <7109145+fred-corp@users.noreply.github.com>
2022-12-01 15:03:11 +0100

    completed part 2

--- Challenges/day01/calorieCounter.py
+++ Challenges/day01/calorieCounter.py
@@ -1,8 +1,12 @@
 # Advent of Code 2022: Day 1: Calorie counting
 # https://adventofcode.com/2022/day/1
 #
+# Part one :
 # From a text file, make the sum of the group of numbers separated by a blank line
 # and output the largest sum with the number of the group
+#
+# Part two :
+# From a text file, make the sum of the 3 groups of numbers with the largest
 #
 # Solution by Frédéric Druppel
 # See repo for license
@@ -35,7 +39,34 @@ def findLargestGroup(fileName):
   
   return maxSum, maxGroup
 
+@timeit
+def sum3LargestGroups(filename):
+  # Read the lines of the input file
+  with open(filename, "r") as f:
+    lines = f.readlines()
+
+  sums = []
+  sum = 0
+  group = 0
+  for line in lines:
+    # Create a new group if the line is empty
+    if line == "\n":
+      sums.append(sum)
+      sum = 0
+      group += 1
+    else:
+      # Remove the newline character and add the number to the sum
+      sum += int(line.strip())
+  sums.append(sum)
+
+  sums.sort(reverse=True)
+
+  return sums[0] + sums[1] + sums[2], sums[0], sums[1], sums[2]
+
+
 if __name__ == "__main__":
   fileName = sys.argv[1]
   maxSum, maxGroup = findLargestGroup(fileName)
   print("The largest sum is {} in group {}".format(maxSum, maxGroup))
+  sum3, g1, g2, g3 = sum3LargestGroups(fileName)
+  print("The sum of the 3 largest groups is {} ({} + {} + {})".format(sum3, g1, g2, g3))
