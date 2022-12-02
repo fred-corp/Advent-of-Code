8acb3a8ee1cdf7d75c48b08b65a4fd1558fb562a
fred-corp <7109145+fred-corp@users.noreply.github.com>
2022-12-02 10:34:27 +0100

    completed part two of day 2 challenge

--- Challenges/day02/strategy.py
+++ Challenges/day02/strategy.py
@@ -10,6 +10,13 @@
 # If a player wins, he gets 6 points, if he loses, he gets 0 points, if it's a draw, he gets 3 points.
 # Also, if a player plays rock he gets 1 point, if he plays paper he gets 2 points, if he plays scissors he gets 3 points, regardless of the outcome.
 # 
+#
+# Part two :
+# The strategy has changed; X means you need to lose, Y means you need to draw and Z means you need to win.
+# The score is calculated in the same way as before.
+#
+#
+#
 # Solution by Frédéric Druppel
 # See repo for license
 
@@ -28,7 +35,7 @@ def countScore(fileName):
   for line in lines:
     # Remove the newline character and split the line into the moves of the players
     moves = line.strip().split(" ")
-    
+
     # If the moves are the same, the score is 3
     if RPC_LUT[moves[0]] == RPC_LUT[moves[1]]:
       score += 3
@@ -41,7 +48,64 @@ def countScore(fileName):
 
   return score
 
+
+@timeit
+def countScore2(fileName):
+  with open(fileName, "r") as f:
+    lines = f.readlines()
+
+  score = 0
+  for line in lines:
+    # Remove the newline character and split the line into the moves of the players
+    moves = line.strip().split(" ")
+
+    # If the strategy is to lose
+    if moves[1] == 'X':
+      # If player 0 plays rock, player 1 plays scissors
+      if moves[0] == 'A':
+        score += 3
+      # If player 0 plays paper, player 1 plays rock
+      elif moves[0] == 'B':
+        score += 1
+      # If player 0 plays scissors, player 1 plays paper
+      elif moves[0] == 'C':
+        score += 2
+
+    # If the strategy is to draw
+    elif moves[1] == 'Y':
+      score += 3
+      # If player 0 plays rock, player 1 plays rock
+      if moves[0] == 'A':
+        score += 1
+      # If player 0 plays paper, player 1 plays paper
+      elif moves[0] == 'B':
+        score += 2
+      # If player 0 plays scissors, player 1 plays scissors
+      elif moves[0] == 'C':
+        score += 3
+
+    # If the strategy is to win
+    elif moves[1] == 'Z':
+      score += 6
+      # If player 0 plays rock, player 1 plays paper
+      if moves[0] == 'A':
+        score += 2
+      # If player 0 plays paper, player 1 plays scissors
+      elif moves[0] == 'B':
+        score += 3
+      # If player 0 plays scissors, player 1 plays rock
+      elif moves[0] == 'C':
+        score += 1
+
+  return score
+
+
+
 if __name__ == "__main__":
   print("Part one :")
   print("The score is", countScore(sys.argv[1]))
   print()
+
+  print("Part two :")
+  print("The score is", countScore2(sys.argv[1]))
+
