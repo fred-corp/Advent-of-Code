7a527aac5f83e286d6ae6f535e2bc9ae83d10b80
fred-corp <7109145+fred-corp@users.noreply.github.com>
2023-12-03 09:21:22 +0100

    coded part 2

--- 2023/day03/day03.js
+++ 2023/day03/day03.js
@@ -20,6 +20,9 @@
 // .664.598..
 //
 // Part two :
+// This time, find all the "*" that are adjacent to exactly two numbers
+// and multiply those numbers together. For example, the 1st symbol in
+// the above example would have a value of 467*35 = 16345.
 //
 //
 // Solution by Frédéric Druppel
@@ -115,7 +118,7 @@ function getValue(prevLine, currentLine, nextLine, j) {
 }
 
 
-function calcSymbol(lines) {
+function calcSymbol(lines, partTwo = false) {
   let sum = 0
   for (let i = 1; i < lines.length-1; i++) {
     const prevLine = lines[i-1]
@@ -129,7 +132,12 @@ function calcSymbol(lines) {
 
       // Check if currentChar is a number
       if (!currentChar.match(/\d/) && currentChar !== ".") {
-        symbolValue = getValue(prevLine, currentLine, nextLine, j)
+        if (partTwo) {
+          symbolValue = findRatio(prevLine, currentLine, nextLine, j)
+        }
+        else {
+          symbolValue = getValue(prevLine, currentLine, nextLine, j)
+        }
         //console.log(`Symbol: ${currentChar}, value: ${symbolValue}, i: ${i+1}, j: ${j+1}`)
       }
       sum += symbolValue
@@ -154,10 +162,122 @@ timedAnswerPartOne()
 
 console.log()
 
+function findGears(line, j) {
+  let gears = []
+  let symbolValue = 0
+
+  let prevPossible = true
+  let midPossible = true
+  let nextPossible = true
+
+  if (/^\d$/.test(line[j-1]) && prevPossible) {
+    midPossible = false
+    if (/^\d$/.test(line[j])) {
+      nextPossible = false
+      if (/^\d$/.test(line[j-2])) {
+        symbolValue = parseInt(line[j-2] + line[j-1] + line[j])
+        gears.push(symbolValue)
+      }
+      else {
+        if (/^\d$/.test(line[j+1])) {
+          symbolValue = parseInt(line[j-1] + line[j] + line[j+1])
+          gears.push(symbolValue)
+        }
+        else {
+          symbolValue = parseInt(line[j-1] + line[j])
+          gears.push(symbolValue)
+        }
+      }
+    }
+    else {
+      if (/^\d$/.test(line[j-2])) {
+        if (/^\d$/.test(line[j-3])) {
+          symbolValue = parseInt(line[j-3] + line[j-2] + line[j-1])
+          gears.push(symbolValue)
+        }
+        else {
+          symbolValue = parseInt(line[j-2] + line[j-1])
+          gears.push(symbolValue)
+        }
+      }
+      else {
+        symbolValue = parseInt(line[j-1])
+        gears.push(symbolValue)
+      }
+    }
+  }
+
+  if (/^\d$/.test(line[j]) && midPossible) {
+    nextPossible = false
+    if (/^\d$/.test(line[j+1])) {
+      if (/^\d$/.test(line[j-1])) {
+        symbolValue = parseInt(line[j-1] + line[j] + line[j+1])
+        gears.push(symbolValue)
+      }
+      else {
+        if (/^\d$/.test(line[j+2])) {
+          symbolValue = parseInt(line[j] + line[j+1] + line[j+2])
+          gears.push(symbolValue)
+        }
+        else {
+          symbolValue = parseInt(line[j] + line[j+1])
+          gears.push(symbolValue)
+        }
+      }
+    }
+    else {
+      symbolValue = parseInt(line[j])
+      gears.push(symbolValue)
+    }
+  }
+
+  if (/^\d$/.test(line[j+1]) && nextPossible) {
+    if (/^\d$/.test(line[j+2])) {
+      if (/^\d$/.test(line[j+3])) {
+        symbolValue = parseInt(line[j+1] + line[j+2] + line[j+3])
+        gears.push(symbolValue)
+      }
+      else {
+        symbolValue = parseInt(line[j+1] + line[j+2])
+        gears.push(symbolValue)
+      }
+    }
+    else {
+      symbolValue = parseInt(line[j+1])
+      gears.push(symbolValue)
+    }
+  }
+  return gears
+}
+
+function findRatio(prevLine, currentLine, nextLine, j) {
+  if (currentLine[j] !== "*") {
+    return 0
+  }
+  let gearsPrevious = findGears(prevLine, j)
+  let gearsCurrent = findGears(currentLine, j)
+  let gearsNext = findGears(nextLine, j)
+  let ratio = 0
+
+  // remove 0 values from gears
+  gearsPrevious = gearsPrevious.filter(x => x !== 0)
+  gearsCurrent = gearsCurrent.filter(x => x !== 0)
+  gearsNext = gearsNext.filter(x => x !== 0)
+
+  console.log(`gearsPrevious: ${gearsPrevious}, gearsCurrent: ${gearsCurrent}, gearsNext: ${gearsNext}`)
+
+  // check if there are exactly two gears in total
+  if (gearsPrevious.length + gearsCurrent.length + gearsNext.length === 2) {
+    ratio = gearsPrevious.reduce((a, b) => a * b, 1) * gearsCurrent.reduce((a, b) => a * b, 1) * gearsNext.reduce((a, b) => a * b, 1)
+  }
+  console.log(`ratio: ${ratio}`)
+  return ratio
+}
+
 function answerPartTwo() {
   fileName = process.argv[2] ? process.argv[2] : "puzzleInputTest.txt"
   const lines = parseFile(fileName)
-  console.log("Part two code goes here")
+  console.log(calcSymbol(lines, true))
 }
 
 console.log("Part two:")
