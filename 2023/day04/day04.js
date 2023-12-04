975b3e1e6c4238f4ea42507abbffe6bce8244d80
fred-corp <7109145+fred-corp@users.noreply.github.com>
2023-12-04 06:35:26 +0100

    coded part 1

--- 2023/day04/day04.js
+++ 2023/day04/day04.js
@@ -2,7 +2,12 @@
 // https://adventofcode.com/2023/day/4
 //
 // Part one :
-//
+// From a file with lines that look like this:
+// Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+// extract the first list of numbers and the second list of numbers
+// and check if the secont list has numbers that are in the first list.
+// THe card is worth 1 for the first match, then doubled three times 
+// for each of the three matches after the first
 //
 // Part two :
 //
@@ -10,8 +15,10 @@
 // Solution by Frédéric Druppel
 // See repo for license
 
+const { get } = require('http')
 const timeIt = require('../timeIt')
 const fs = require('fs')
+const { match } = require('assert')
 
 function parseFile(textFile) {
   const text = fs.readFileSync(textFile, 'utf8')
@@ -19,10 +26,48 @@ function parseFile(textFile) {
   return lines
 }
 
+function parseLine(line) {
+  const card = line.split(':')
+  const [card1, card2] = card[1].split('|')
+  let card1Numbers = card1.split(' ').map(x => parseInt(x))
+  let card2Numbers = card2.split(' ').map(x => parseInt(x))
+
+  // remove NaNs
+  card1Numbers = card1Numbers.filter(x => !isNaN(x))
+  card2Numbers = card2Numbers.filter(x => !isNaN(x))
+
+  return [card1Numbers, card2Numbers]
+}
+
+function checkForMatches(card1, card2) {
+  let matches = 0
+  let score = 0
+  for (let i = 0; i < card1.length; i++) {
+    for (let j = 0; j < card2.length; j++) {
+      if (card1[i] === card2[j]) {
+        matches++
+        if (score === 0) { score = 1 } 
+        else { score *= 2 }
+      }
+    }
+  }
+  return [matches, score]
+}
+
+function getMatches(lines){
+  let score = 0
+  for (let i = 0; i < lines.length; i++) {
+    const [card1, card2] = parseLine(lines[i])
+    score += checkForMatches(card1, card2)[1]
+  }
+  return score
+
+}
+
 function answerPartOne() {
   fileName = process.argv[2] ? process.argv[2] : "puzzleInputTest.txt"
   const lines = parseFile(fileName)
-  console.log("Part one code goes here")
+  console.log(getMatches(lines))
 }
 
 console.log("Part one:")
