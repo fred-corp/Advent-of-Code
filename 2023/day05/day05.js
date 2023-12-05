0eca0569dcca3577bb5e566b8ac60987fe34ae66
fred-corp <7109145+fred-corp@users.noreply.github.com>
2023-12-05 07:10:26 +0100

    coded part 1 of day 5

--- 2023/day05/day05.js
+++ 2023/day05/day05.js
@@ -15,14 +15,77 @@ const fs = require('fs')
 
 function parseFile(textFile) {
   const text = fs.readFileSync(textFile, 'utf8')
-  const lines = text.split('\n')
-  return lines
+  // parse the file at blank lines into groups of lines
+  const lines = text.split(/\r?\n/)
+  // create a group of elements splitted at each '' in the lines array
+  const groups = lines.reduce((groups, line) => {
+    if (line.length === 0) {
+      groups.push([])
+    } else {
+      groups[groups.length - 1].push(line)
+    }
+    return groups
+  }, [[]])
+  return groups
+}
+
+function getSeeds(seedsLine) {
+  let seeds = []
+  seeds = seedsLine[0].split(' ').slice(1)
+  seeds = seeds.map(Number)
+  return seeds
+}
+
+function getSourceToDestMap(group) {
+  let sourceDest = []
+  group.slice(1).forEach(element => {
+    let numbers = element.split(' ')
+    numbers = numbers.map(Number)
+    sourceDest.push(numbers)
+  });
+  return sourceDest
+}
+
+function convertToDest(seeds, sourceDestArrays) {
+  let dests = []
+  seeds.forEach(seed => {
+    let val = seed
+    let newVal = val
+    sourceDestArrays.forEach(sourceDestArray => {
+      newVal = val
+      let stop = false
+      sourceDestArray.forEach(sourceDest => {
+        let dest = sourceDest[0]
+        let source = sourceDest[1]
+        let range = sourceDest[2]
+        if (source <= val && val <= source + range && !stop) {
+          newVal = val - source + dest
+          stop = true
+        }
+      })
+      val = newVal
+    })
+    dests.push(newVal)
+  })
+  return dests
+}
+
+function mapSeedsToLocations(groups) {
+  let seeds = getSeeds(groups[0])
+  let sourceDestArrays = []
+  groups.slice(1).forEach(group => {
+    sourceDestArrays.push(getSourceToDestMap(group))
+  })
+  let dests = convertToDest(seeds, sourceDestArrays)
+  return dests
 }
 
 function answerPartOne() {
   fileName = process.argv[2] ? process.argv[2] : "puzzleInputTest.txt"
-  const lines = parseFile(fileName)
-  console.log("Part one code goes here")
+  const groups = parseFile(fileName)
+  const locations = mapSeedsToLocations(groups)
+  const min = Math.min(...locations)
+  console.log(min)
 }
 
 console.log("Part one:")
