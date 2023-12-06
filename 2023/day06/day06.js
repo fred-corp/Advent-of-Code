8f6d27bf21d194bc7cb7b95470c3a1c050995462
fred-corp <7109145+fred-corp@users.noreply.github.com>
2023-12-06 07:46:53 +0100

    coded part 1

--- 2023/day06/day06.js
+++ 2023/day06/day06.js
@@ -1,8 +1,13 @@
 // Advent of Code 2023: Day 6: Wait For It
 // https://adventofcode.com/2023/day/6
 //
+// Input :
+// Time:      7  15   30
+// Distance:  9  40  200
+
 // Part one :
-//
+// Distance = (Time - holdTime)*holdTime
+// Calculate the amount of ways to get to farther than the record distance
 //
 // Part two :
 //
@@ -19,10 +24,50 @@ function parseFile(textFile) {
   return lines
 }
 
+function getGames(lines) {
+  let games = []
+  let times = []
+  lines.forEach(line => {
+    times.push(line.split(' ')
+                      .slice(1)
+                      .map(time => parseInt(time))
+                      .filter(time => !isNaN(time))
+                      .filter(time => time !== undefined)
+                      )
+  })
+  for (let i = 0; i < times[0].length; i++) {
+    games.push([times[0][i], times[1][i]])
+  }
+  return games
+}
+
+function getDistance(time, holdTime) {
+  return (time - holdTime) * holdTime
+}
+
+function getAmountOfWays(game) {
+  let ways = 0
+  for (let i = 0; i < game[0]; i++) {
+    if (getDistance(game[0], i) > game[1]) {
+      ways++
+    }
+  }
+  return ways
+}
+
+function getAmountOfWaysProduct(games) {
+  let product = 1
+  games.forEach(game => {
+    product *= getAmountOfWays(game)
+  })
+  return product
+}
+
 function answerPartOne() {
   const fileName = process.argv[2] ? process.argv[2] : "puzzleInputTest.txt"
   const lines = parseFile(fileName)
-  console.log("Part one code goes here")
+  const games = getGames(lines)
+  console.log(getAmountOfWaysProduct(games))
 }
 
 console.log("Part one:")
