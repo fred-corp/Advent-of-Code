fb45756907a230029d71f5b6c54846ffcff09e44
fred-corp <7109145+fred-corp@users.noreply.github.com>
2023-12-06 07:24:10 +0100

    added files for day 6

---
+++ 2023/day06/README.md
@@ -0,0 +1,25 @@
+# --- Day 6: \<name of challenge\> ---
+
+## --- Part One ---
+
+> Part one goes here
+
+## --- Part Two ---
+
+> Part two goes here
+
+## Solution
+
+The challenge was done in JavaScript, with a timer decorator to measure the execution time.
+
+To run the solution, you'll need a javascript interpreter. I used Node.js v21.2.0
+
+```zsh
+node day06.js puzzleInput.txt
+```
+
+The output (with the specified dataset) should be:
+
+```zsh
+
+```
---
+++ 2023/day06/day06.js
@@ -0,0 +1,42 @@
+// Advent of Code 2023: Day 6:
+// https://adventofcode.com/2023/day/6
+//
+// Part one :
+//
+//
+// Part two :
+//
+//
+// Solution by Frédéric Druppel
+// See repo for license
+
+const timeIt = require('../timeIt')
+const fs = require('fs')
+
+function parseFile(textFile) {
+  const text = fs.readFileSync(textFile, 'utf8')
+  const lines = text.split('\n')
+  return lines
+}
+
+function answerPartOne() {
+  const fileName = process.argv[2] ? process.argv[2] : "puzzleInputTest.txt"
+  const lines = parseFile(fileName)
+  console.log("Part one code goes here")
+}
+
+console.log("Part one:")
+const timedAnswerPartOne = timeIt(answerPartOne)
+timedAnswerPartOne()
+
+console.log()
+
+function answerPartTwo() {
+  const fileName = process.argv[2] ? process.argv[2] : "puzzleInputTest.txt"
+  const lines = parseFile(fileName)
+  console.log("Part two code goes here")
+}
+
+console.log("Part two:")
+const timedAnswerPartTwo = timeIt(answerPartTwo)
+timedAnswerPartTwo()
