066c2f73861b3d9fdc781adc035cf98166e49af0
fred-corp <7109145+fred-corp@users.noreply.github.com>
2023-12-08 06:27:39 +0100

    coded part 1

--- 2023/day08/day08.js
+++ 2023/day08/day08.js
@@ -19,10 +19,45 @@ function parseFile(textFile) {
   return lines
 }
 
+function getNodes(lines) {
+  let instructions = lines[0].split('')
+  // replace 'R' by 1 and 'L' by 0
+  instructions = instructions.map(i => i === 'R' ? 1 : 0)
+  var nodes = {}
+  lines.shift()
+  lines.shift()
+  lines.forEach(line => {
+    line = line.replace(/\s/g, '')
+    const keyValue = line.split('=')
+    const node = keyValue[0]
+    const value = keyValue[1].replace(/\(|\)/g,'').split(",")
+    nodes[node] = value
+  })
+  return [instructions, nodes]
+}
+
+function getNavigationSteps(instructions, nodes) {
+  let navigationSteps = 0
+  let instructionsIndex = 0
+  let currentNode = 'AAA'
+  while (currentNode !== 'ZZZ') {
+    const nextNode = nodes[currentNode][instructions[instructionsIndex]]
+    currentNode = nextNode
+    instructionsIndex++
+    if (instructionsIndex >= instructions.length) {
+      instructionsIndex = 0
+    }
+    navigationSteps++
+  }
+  return navigationSteps
+}
+
 function answerPartOne() {
   const fileName = process.argv[2] ? process.argv[2] : "puzzleInputTest.txt"
   const lines = parseFile(fileName)
-  console.log("Part one code goes here")
+  const [instructions, nodes] = getNodes(lines)
+  const navigationSteps = getNavigationSteps(instructions, nodes)
+  console.log(navigationSteps)
 }
 
 console.log("Part one:")
