0c2a7dfcc13b9998d62eb510414a19e858afb304
fred-corp <7109145+fred-corp@users.noreply.github.com>
2023-12-11 06:54:48 +0100

    coded part 2 added a way to calculate without expanding the universe (so the heap doesn't run out)

--- 2023/day11/day11.js
+++ 2023/day11/day11.js
@@ -27,13 +27,14 @@ function createUniverse(lines) {
   return universe
 }
 
-function expandUniverse(universe) {
+function expandUniverse(universe, amount=2) {
   // double each rows that contain only '.'s
   let newUniverse = []
   universe.forEach((row) => {
     if (row.every((seat) => seat === '.')) {
-      newUniverse.push(row)
-      newUniverse.push(row)
+      for (let i = 0; i < amount; i++) {
+        newUniverse.push(row)
+      }
     } else {
       newUniverse.push(row)
     }
@@ -44,8 +45,9 @@ function expandUniverse(universe) {
   let newNewUniverse = []
   newUniverse.forEach((col) => {
     if (col.every((seat) => seat === '.')) {
-      newNewUniverse.push(col)
-      newNewUniverse.push(col)
+      for (let i = 0; i < amount; i++) {
+        newNewUniverse.push(col)
+      }
     } else {
       newNewUniverse.push(col)
     }
@@ -122,10 +124,64 @@ timedAnswerPartOne()
 
 console.log()
 
+function findExpandedRowsCols(universe) {
+  let expandedRows = []
+  let expandedCols = []
+  universe.forEach((row, rowIndex) => {
+    if (row.every((seat) => seat === '.')) {
+      expandedRows.push(rowIndex)
+    }
+  })
+  universe = universe[0].map((col, i) => universe.map(row => row[i]))
+  universe.forEach((col, colIndex) => {
+    if (col.every((seat) => seat === '.')) {
+      expandedCols.push(colIndex)
+    }
+  })
+  return [expandedRows, expandedCols]
+}
+
+function calcDistanceWithoutExpanding(galaxyPair, expandedRowsCols, amount) {
+  const x1 = Math.min(galaxyPair[0][0], galaxyPair[1][0])
+  const y1 = Math.min(galaxyPair[0][1], galaxyPair[1][1])
+  const x2 = Math.max(galaxyPair[0][0], galaxyPair[1][0])
+  const y2 = Math.max(galaxyPair[0][1], galaxyPair[1][1])
+  const verticalDistance = x2 - x1
+  const horizontalDistance = y2 - y1
+  let distance = verticalDistance + horizontalDistance
+  // if any expandedRow is between the two galaxies, add the amount to the distance
+  expandedRowsCols[0].forEach(expandedRow => {
+    if (x1 < expandedRow && x2 > expandedRow) {
+      distance += amount - 1
+    }
+  })
+  // same for expandedCols
+  expandedRowsCols[1].forEach(expandedCol => {
+    if (y1 < expandedCol && y2 > expandedCol) {
+      distance += amount - 1
+    }
+  })
+
+  return distance
+}
+
+function findDistanceSumsWithoutExpanding(galaxyPairs, expandedRowsCols, amount) {
+  let distances = []
+  galaxyPairs.forEach((pair) => {
+    distances.push(calcDistanceWithoutExpanding(pair, expandedRowsCols, amount))
+  })
+  return distances.reduce((a, b) => a + b, 0)
+}
+
 function answerPartTwo() {
   const fileName = process.argv[2] ? process.argv[2] : "puzzleInputTest.txt"
   const lines = parseFile(fileName)
-  console.log("Part two code goes here")
+  let universe = createUniverse(lines)
+  const galaxies = findGalaxies(universe)
+  const galaxyPairs = getGalaxyPairs(galaxies)
+  const expandedRowsCols = findExpandedRowsCols(universe)
+  const distanceSums = findDistanceSumsWithoutExpanding(galaxyPairs, expandedRowsCols, 1000000)
+  console.log(distanceSums)
 }
 
 console.log("Part two:")
