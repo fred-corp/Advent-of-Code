3538aa08eda737acc1d0b4443cfdf01c335181a6
fred-corp <7109145+fred-corp@users.noreply.github.com>
2023-12-12 06:35:20 +0100

    coded part 1 (brute-force)

--- 2023/day12/day12.js
+++ 2023/day12/day12.js
@@ -19,10 +19,65 @@ function parseFile(textFile) {
   return lines
 }
 
+function checkArrangement(arrangement, springsKnown) {
+  // convert a string of '#'s and '.' to an array of numbers
+  // counting the groups of contiguous '#'s
+  // and compare it to the array of known springs
+  let springsFound = []
+  let currentSpring = 0
+  arrangement += '.'
+  for (let i = 0; i < arrangement.length; i++) {
+    if (arrangement[i] === '#') {
+      currentSpring++
+    } else {
+      if (currentSpring > 0) {
+        springsFound.push(currentSpring)
+        currentSpring = 0
+      }
+    }
+  }
+  return springsFound.join(',') === springsKnown.join(',')
+}
+
+function findArrangements(line) {
+  let [springsUnknown, springsKnown] = line.split(' ')
+  springsKnown = springsKnown.split(',').map(spring => parseInt(spring))
+
+  let arrangements = 0
+  // replace each '?' with '.' or '#', and count the amount of '#'s separated by '.'s;
+  // these amounts are the possible arrangements and should be equal to springsKnown
+
+  let unknownAmount = springsUnknown.split('').filter(char => char === '?').length
+  for (let i = 0; i < Math.pow(2, unknownAmount); i++) {
+    let arrangement = springsUnknown
+    for (let j = 0; j < springsUnknown.length; j++) {
+      if (i & (1 << j)) {
+        arrangement = arrangement.replace('?', '#')
+      } else {
+        arrangement = arrangement.replace('?', '.')
+      }
+    }
+    if (checkArrangement(arrangement, springsKnown)) {
+      arrangements++
+    }
+  }
+  return arrangements
+
+}
+
+function getArrangementsSum(lines) {
+  let arrangementsSum = 0
+  for (let line of lines) {
+    arrangementsSum += findArrangements(line)
+  }
+  return arrangementsSum
+}
+
 function answerPartOne() {
   const fileName = process.argv[2] ? process.argv[2] : "puzzleInputTest.txt"
   const lines = parseFile(fileName)
-  console.log("Part one code goes here")
+  const arrangementsSum = getArrangementsSum(lines)
+  console.log(arrangementsSum)
 }
 
 console.log("Part one:")
