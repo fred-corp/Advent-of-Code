aee61d1d78c8edb65792d410904293daa82cb78d
fred-corp <Frederic.druppel@icloud.com>
2024-12-03 13:22:04 +0100

    forgot to add day17 of 2023

--- 2023/day17/day17.js
+++ 2023/day17/day17.js
@@ -2,7 +2,9 @@
 // https://adventofcode.com/2023/day/17
 //
 // Part one :
-//
+// From a map with weighed tiles, find the path with the least weight.
+// It is no possible to go more than 3 tiles in the same direction.
+// You start from the top left corner and must reach the bottom right corner.
 //
 // Part two :
 //
@@ -10,6 +12,7 @@
 // Solution by Frédéric Druppel
 // See repo for license
 
+const { get } = require('http')
 const timeIt = require('../timeIt')
 const fs = require('fs')
 
@@ -19,10 +22,80 @@ function parseFile(textFile) {
   return lines
 }
 
+function getMap(lines) {
+  const map = {}
+  lines.forEach((line, x) => {
+    line.split('').forEach((char, y) => {
+      map[[x, y]] = parseInt(char)
+    })
+  })
+  return map
+}
+
+function dijkstra(grid, startPosition, endPosition, maxDist) {
+  const start = [startPosition, [0, 0]];
+  const distance = new Map([[start, 0]]);
+  const pathQueue = [[0, start]];
+
+  while (pathQueue.length > 0) {
+    const [_, tile] = pathQueue.shift();
+    if (!tile[1].every((n) => -maxDist <= n && n <= maxDist)) {
+      continue;
+    }
+    if (tile[0] === endPosition) {
+      return distance.get(tile);
+    }
+    for (const v of getNeighbors(tile)) {
+      if (!grid[v[0]]) {
+        continue;
+      }
+      const alt = distance.get(tile) + grid[v[0]];
+      if (alt < (distance.get(v) || Infinity)) {
+        distance.set(v, alt);
+        pathQueue.push([alt, v]);
+        pathQueue.sort((a, b) => a[0] - b[0]);
+      }
+    }
+  }
+}
+
+function getNeighbors(tile) {
+  const [position, direction] = tile;
+  const [x, y] = position;
+  const [dx, dy] = direction;
+  const neighbors = [];
+  if (dx === 0) {
+    neighbors.push([[x - 1, y], [-1, 0]]);
+    neighbors.push([[x + 1, y], [1, 0]]);
+  }
+  if (dy === 0) {
+    neighbors.push([[x, y - 1], [0, -1]]);
+    neighbors.push([[x, y + 1], [0, 1]]);
+  }
+  if (dx > 0) {
+    neighbors.push([[x + 1, y], [1, 0]]);
+  }
+  if (dx < 0) {
+    neighbors.push([[x - 1, y], [-1, 0]]);
+  }
+  if (dy > 0) {
+    neighbors.push([[x, y + 1], [0, 1]]);
+  }
+  if (dy < 0) {
+    neighbors.push([[x, y - 1], [0, -1]]);
+  }
+  return neighbors;
+
+}
+
 function answerPartOne() {
   const fileName = process.argv[2] ? process.argv[2] : "puzzleInputTest.txt"
   const lines = parseFile(fileName)
-  console.log("Part one code goes here")
+  const map = getMap(lines)
+  const start = [0, 0]
+  const end = [lines.length - 1, lines[0].length - 1]
+  const visited = dijkstra(map, start, end, 3)
+  console.log(visited)
 }
 
 console.log("Part one:")
